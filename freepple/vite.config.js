13d60004c29a381030d52efc44b5c1c0e35ba990
dylan <mixedberries92@gmail.com>
2026-01-07 12:50:09 +0800

    More setup

--- freepple/vite.config.js
+++ freepple/vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  nodePolyfills({
+      // To ensure XRPL.js works in the browser
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+      protocolImports: true,
+    }),
+    tailwindcss(),
+  ],
+  resolve: {
+  alias: {
+    stream: 'stream-browserify',
+  },
+}, 
 })
